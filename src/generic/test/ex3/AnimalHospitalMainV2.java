8bb7dc97381da6b6873940ab63437be785d941f0
minyugyeong <alsdbrud123@naver.com>
2024-07-24 14:00:49 +0900

    섹션 2 제네릭(Generic2) - 타입 매개변수 제한3(제네릭 도입과 실패)

---
+++ src/generic/test/ex3/AnimalHospitalMainV2.java
@@ -0,0 +1,15 @@
+package generic.test.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV2 {
+
+    public static void main(String[] args) {
+        AnimalHospitalV2<Dog> dogHospital = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Cat> catHospital = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Integer> integerHospital = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Object> objectHospital = new AnimalHospitalV2<>();
+        // 별의 별 게 다 들어올 수 있다.. 동물만 쓰고 싶은데..
+    }
+}
