522f77bb2aa5ba7894bf77971428c983a0f212ee
minyugyeong <alsdbrud123@naver.com>
2024-07-24 14:20:36 +0900

    섹션 2 제네릭(Generic2) - 제네릭 메서드

---
+++ src/generic/test/ex4/GenericMethod.java
@@ -0,0 +1,21 @@
+package generic.test.ex4;
+
+import java.sql.SQLOutput;
+
+public class GenericMethod {
+
+    public static Object objMethod(Object obj) {
+        System.out.println("Object print: " + obj);
+        return obj;
+    }
+
+    public static <T> T genericMethod(T t) {
+        System.out.println("generic print: " + t);
+        return t;
+    }
+
+    public static <T extends Number> T numberMethod(T t) {
+        System.out.println("bound print: " + t);
+        return t;
+    }
+}
