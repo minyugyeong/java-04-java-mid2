57799af98cc78c3b8987fbcdd02e87c4f14fc70d
minyugyeong <alsdbrud123@naver.com>
2024-07-25 16:24:03 +0900

    섹션 3 컬렉션 프레임워크(LinkedList) - 노드와 연결1

---
+++ src/collection/link/NodeMain1.java
@@ -0,0 +1,17 @@
+package collection.link;
+
+public class NodeMain1 {
+
+    public static void main(String[] args) {
+        //노드 생성하고 연결하기: A -> B -> C
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        Node x = first;
+        while (x != null) {
+            System.out.println(x.item);
+            x = x.next;
+        }
+    }
+}
