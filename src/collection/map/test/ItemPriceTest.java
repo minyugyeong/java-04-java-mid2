800d77edf9fb3bf62007861894536ee8deb66d99
minyugyeong <alsdbrud123@naver.com>
2024-08-16 13:22:34 +0900

    섹션 9 컬렉션 프레임워크(Map, Stack, Queue) - 문제와 풀이1(Map1)

---
+++ src/collection/map/test/ItemPriceTest.java
@@ -0,0 +1,27 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ItemPriceTest {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("사과", 500);
+        map.put("바나나", 500);
+        map.put("망고", 1000);
+        map.put("딸기", 1000);
+
+        Set<String> set = new HashSet<>();
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1000) {
+                set.add(key);
+            }
+        }
+
+        System.out.println(set);
+
+    }
+}
